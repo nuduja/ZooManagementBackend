8d859a6c8b61c9c561f0841ab06a6e761525dd31
nuduja <nudujabandara@gmail.com>
2024-01-28 19:45:32 +0530

    initial commit

---
+++ src/main/java/com/fulltack/zooManagment/ZooManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.fulltack.zooManagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZooManagmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fulltack/zooManagment/ZooManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fulltack.zooManagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

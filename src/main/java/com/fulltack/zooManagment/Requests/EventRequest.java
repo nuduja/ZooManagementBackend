b3778563b0f0204866a6a964819bb95bd7341b12
nuduja <nudujabandara@gmail.com>
2024-04-23 23:37:57 +0530

    Add Event Request No arg constructor

--- src/main/java/com/fulltack/zooManagment/Requests/EventRequest.java
+++ src/main/java/com/fulltack/zooManagment/Requests/EventRequest.java
@@ -2,12 +2,14 @@ package com.fulltack.zooManagment.Requests;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 
 import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class EventRequest {
 
     @NotNull
